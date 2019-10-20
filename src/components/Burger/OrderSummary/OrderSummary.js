3b00c345c33470ab3f2cd2da39cf8f1000b252a4
Buntybru <buntybru@gmail.com>
2019-10-20 23:55:25 +0530

    Push after many days, working model done

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class orderSummary extends Component {
+    componentWillUpdate()
+    {
+        console.log("Order summary will update");
+    }
+   
 
-    const ingredientSummary =Object.keys(props.ingredients)
-    .map(igKey => {
-        return(  <li key= {igKey}> 
-        <span style={{textTransform:'capitalize'}}>
-        {igKey}
-        </span> : {props.ingredients[igKey]}
-        </li>)
-    });
-
-    return (
-        <Aux>
+    render()
+    {
+        const ingredientSummary =Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return(  <li key= {igKey}> 
+            <span style={{textTransform:'capitalize'}}>
+            {igKey}
+            </span> : {this.props.ingredients[igKey]}
+            </li>)
+        });
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
+            <p><strong>Total Price: {this.props.price}</strong></p>
             <p>Continue to checkout ?</p>
             <Button
             btnType = 'Danger'
-            clicked = {props.purchaseCancelled}>
+            clicked = {this.props.purchaseCancelled}>
             Cancel
             </Button>
             <Button
-            clicked={props.purchaseContinued}
+            clicked={this.props.purchaseContinued}
             btnType = 'Success'
             >Continue</Button>
         </Aux>
-    )
 
-};
+
+        )
+    }
+}
+
+
 
 export default orderSummary;
